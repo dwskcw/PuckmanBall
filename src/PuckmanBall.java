d3a7092f68430c81f217b7bc71a953b805178fd8
zbrotherton <zanebrotherton@gmail.com>
2024-11-09 12:48:02 -0500

    table

--- src/PuckmanBall.java
+++ src/PuckmanBall.java
@@ -23,7 +23,23 @@ public class PuckmanBall {
 
 			ClearBackground(LIGHTGRAY);
 			
-			DrawText("Puckman", 190, 200, 20, RAYWHITE);
+			DrawRectangle(10, 10, screenWidth - 20, screenHeight - 20, WHITE);
+			DrawRectangle(screenWidth/2 - 2, 10, 4, screenHeight - 20, RED);
+			DrawRectangle((int)(screenWidth * 0.15) - 2, 10, 4, screenHeight- 20, RED);
+			DrawRectangle((int)(screenWidth * 0.85) - 2, 10, 4, screenHeight- 20, RED);
+
+			DrawCircle(screenWidth/2, screenHeight/2, 30, RED);
+			DrawCircle(screenWidth/2, screenHeight/2, 27, WHITE);
+			DrawCircle(screenWidth/2, screenHeight/2, 3, RED);
+
+			DrawRectangle(screenWidth - screenWidth/16, screenHeight / 3, screenWidth/16 - 10, screenHeight/3, RED);
+			DrawRectangle(screenWidth - screenWidth/16 + 3, screenHeight / 3 + 3, screenWidth/16 - 13, screenHeight/3 - 6, WHITE);
+			DrawRectangle(10, screenHeight / 3, screenWidth/16 - 10, screenHeight/3, RED);
+			DrawRectangle(10, screenHeight / 3 + 3, screenWidth/16 - 13, screenHeight/3 - 6, WHITE);
+
+
+			DrawText("Puckman", 190, 200, 20, BLACK);
+
 
 			EndDrawing();
 
